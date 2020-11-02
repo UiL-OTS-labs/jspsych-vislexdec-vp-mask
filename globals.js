1fbe1d23ee6c71e49ce8aaac6d9a666bd259f5d9
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-02 11:26:08 +0100

    Setting the stage for masking the prime...

--- globals.js
+++ globals.js
@@ -1,7 +1,18 @@
 // Default behavior of (sub) trial phases.
 const FIXCROSS_DURATION = 1000;
-const PRIME_DURATION = 500;
+
+const USE_PRE_PRIME_MASK = false;  // true/false: implement pre mask
+const PRE_PRIME_MASK_DURATION = 0; // mask duration in milliseconds
+const DEFAULT_PRE_MASK_CHARACTER = '#'; // the string to be repeated
+
+const PRIME_DURATION = 500; 
+
+const USE_POST_PRIME_MASK = true; // implement post mask
+const POST_PRIME_MASK_DURATION = 80; // mask duration in milliseconds
+const DEFAULT_POST_MASK_CHARACTER = '$'; // the string to be repeated
+
 const WORD_DURATION = 1000;
+
 const RESPONSE_TIMEOUT_DURATION = 2000;
 const FEEDBACK_DURATION = 1000;
 const CROSS_GAP_DURATION = 0;
