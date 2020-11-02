46775a822f715d6227bcddf9cd68e3e785d3f224
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-02 11:09:09 +0100

    Copied globals.js from regular vislexcec-vp.

---
+++ globals.js
@@ -0,0 +1,14 @@
+// Default behavior of (sub) trial phases.
+const FIXCROSS_DURATION = 1000;
+const PRIME_DURATION = 500;
+const WORD_DURATION = 1000;
+const RESPONSE_TIMEOUT_DURATION = 2000;
+const FEEDBACK_DURATION = 1000;
+const CROSS_GAP_DURATION = 0;
+
+// Duration of the interval between prime and target.
+const PRIME_GAP_DURATION = 300;
+
+const WORD_GAP_DURATION = 0;
+const TRIAL_GAP_DURATION = 1000;
+const OK_BUTTON_TEXT = "OK";
