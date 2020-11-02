f221952e44bdef9f5042a9099ac8cb61a956d417
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-02 11:58:14 +0100

    minor doccing in globals.js

--- globals.js
+++ globals.js
@@ -1,12 +1,14 @@
 // Default behavior of (sub) trial phases.
 const FIXCROSS_DURATION = 1000;
 
+//placeholder globaks for programmtic mask generation implementation
 const USE_PRE_PRIME_MASK = false;  // true/false: implement pre mask
 const PRE_PRIME_MASK_DURATION = 0; // mask duration in milliseconds
 const DEFAULT_PRE_MASK_CHARACTER = '#'; // the string to be repeated
 
 const PRIME_DURATION = 500; 
 
+//placeholder globaks for programmtic mask generation implementation
 const USE_POST_PRIME_MASK = true; // implement post mask
 const POST_PRIME_MASK_DURATION = 80; // mask duration in milliseconds
 const DEFAULT_POST_MASK_CHARACTER = '$'; // the string to be repeated
